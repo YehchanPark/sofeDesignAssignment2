ef5ce02003e57355b63b6a242fad58bc145678f6
YehchanPark <yehchan.park@ontariotechu.net>
2021-10-12 13:25:42 -0400

    Added comments

--- code/Exercise1/Product.java
+++ code/Exercise1/Product.java
@@ -1,19 +1,20 @@
 package Exercise1;
 
-public class Product {
-    long UPCCODE;
+public class Product {//Product object
+    long UPCCODE;//UPC code is a long since int does not store enough digits
     String name;
     double price;
-    public Product(String UPCCODE, String name, double price){
+    public Product(String UPCCODE, String name, double price){//Constructor
         this.UPCCODE=Long.parseLong(UPCCODE);
         this.name=name;
         this.price=price;
     }
 
+
+    //Getters
     public long getUPCCODE() {
         return UPCCODE;
     }
-
 
     public String getName() {
         return name;
--- code/Exercise1/productDB.java
+++ code/Exercise1/productDB.java
@@ -1,31 +1,24 @@
 package Exercise1;
 
-public class productDB {
+public class productDB {//Product database class
+    //list of products
     Product apple = new Product("012345678905","Apple",1.00);
     Product grapes = new Product("112345678905","Grapes",1.25);
     Product pear = new Product("212345678905","Pear",1.10);
     Product watermelon = new Product("312345678905","Watermelon",2.00);
     Product lemon = new Product("412345678905","Lemon",1.50);
+    Product[] productList = {apple,grapes,pear,watermelon,lemon};//actual list of products
 
 
-
-
-    public productDB(){
-
-    }
-    Product[] productList = {apple,grapes,pear,watermelon,lemon};
-
-
-
-    public Product GetProductInfo(long UPCCode){
+    public Product GetProductInfo(long UPCCode){//Uses the given upc code to find a products information
     
-        for (int i = 0; i < productList.length; i++) {
-            if(productList[i].getUPCCODE() == UPCCode){
-                return productList[i];
+        for (int i = 0; i < productList.length; i++) {//Checks for each element of the array
+            if(productList[i].getUPCCODE() == UPCCode){//If the given upccode matches the product's upc code
+                return productList[i];//Return that product
             }
 
         }
-       return null;
+       return null;//If it is unable to find the product, it will return a null
     }
 
    }
